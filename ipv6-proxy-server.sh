e75ac48127974b74ea10220045d24288bb7ca1da
2002-115 <85139299+2002-115@users.noreply.github.com>
2024-07-30 18:20:33 +0700

    Update ipv6-proxy-server.sh

--- ipv6-proxy-server.sh
+++ ipv6-proxy-server.sh
@@ -1,58 +1,14 @@
 #!/bin/bash
 
-# Thiết lập môi trường không tương tác
-export DEBIAN_FRONTEND=noninteractive
-
-# Hàm chờ đợi khóa apt/dpkg
-wait_for_lock() {
-    while sudo fuser /var/lib/dpkg/lock-frontend >/dev/null 2>&1; do
-        echo "Đang chờ các trình quản lý gói khác hoàn thành..."
-        sleep 5
-    done
-}
-
-# Chờ đợi khóa apt/dpkg trước khi tiếp tục
-wait_for_lock
-
-# Tạo hoặc chỉnh sửa tệp cấu hình để tắt thông báo và chọn mặc định
-echo 'Dpkg::Options { "--force-confdef"; "--force-confold"; };' | sudo tee /etc/apt/apt.conf.d/90local > /dev/null
-
-echo "Tắt thông báo và chọn mặc định ✅ ✅ ✅ ✅ ✅"
-
-# Đặt trước các câu trả lời cho debconf để tránh yêu cầu tương tác
-echo '* libraries/restart-without-asking boolean true' | sudo debconf-set-selections
-
-# Đặt trước câu trả lời cho việc nâng cấp kernel
-echo 'grub-pc grub-pc/install_devices multiselect /dev/sda' | sudo debconf-set-selections
-
-# Chờ đợi khóa apt/dpkg trước khi tiếp tục
-wait_for_lock
-
-# Cập nhật và nâng cấp hệ thống với các tùy chọn dpkg để giữ bản địa phương của tệp cấu hình
-sudo apt update -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
-sudo apt upgrade -y --allow-downgrades --allow-remove-essential --allow-change-held-packages -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
-
 echo "Cập nhật hệ thống ✅ ✅ ✅ ✅ ✅"
-
-# Chờ đợi khóa apt/dpkg trước khi tiếp tục
-wait_for_lock
-
-# Cài đặt các gói cần thiết với các tùy chọn dpkg để giữ bản địa phương của tệp cấu hình
-sudo apt install wget zip curl openssl -y --allow-downgrades --allow-remove-essential --allow-change-held-packages -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
+sudo apt-get update && sudo apt-get install unzip
 
 echo "Cài đặt các gói cần thiết ✅ ✅ ✅ ✅ ✅"
-
-# Chờ đợi khóa apt/dpkg trước khi tiếp tục
-wait_for_lock
-
+# Cài đặt các gói cần thiết 
+sudo apt install wget zip curl openssl -y --allow-downgrades --allow-remove-essential --allow-change-held-packages -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
 # Chạy script thiết lập mạng từ URL
 curl -sSL https://raw.githubusercontent.com/2002-115/ipv6-debian11-setup_network/main/setup_network.sh | bash
 
-# Chờ đợi khóa apt/dpkg trước khi tiếp tục
-wait_for_lock
-
+echo "Cài đặt proxy ✅ ✅ ✅ ✅ ✅"
 # Chạy script cài đặt proxy từ URL
 wget -qO- https://raw.githubusercontent.com/2002-115/taoipv6-OS-Debian-11-x64-bullseye-/main/custom-ipv6-proxy-server.sh | bash
-
-echo "Cài đặt proxy ✅ ✅ ✅ ✅ ✅"
-
